66012870df2dd386382df5398d343a17b53a6a82
sparloai <ali@human3.co>
2025-12-24 11:08:30 -0600

    hotfix: temporarily remove ExampleReportsSection to debug 502

--- apps/web/app/(marketing)/page.tsx
+++ apps/web/app/(marketing)/page.tsx
@@ -1,15 +1,9 @@
 import { withI18n } from '~/lib/i18n/with-i18n';
 
-import { ExampleReportsSection } from './_components/example-reports/example-reports-section';
 import { SparloHero } from './_components/sparlo-hero';
 
 function Home() {
-  return (
-    <>
-      <SparloHero />
-      <ExampleReportsSection />
-    </>
-  );
+  return <SparloHero />;
 }
 
 export default withI18n(Home);
